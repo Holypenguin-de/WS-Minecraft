8e56c4b7edaf7e4b6ad18f14e79e31fc082e21f8
holypenguin-de <sveny.ziegler@gmail.com>
2022-01-03 23:01:00 +0100

    Add outo Download and config for Spigot and Vanilla

--- index.ts
+++ index.ts
@@ -1,84 +1,100 @@
 import { WebSocketServer } from 'ws';
-import { readFileSync, appendFile, writeFile, closeSync, openSync } from 'fs';
+import { readFileSync, appendFile, writeFile, closeSync, openSync, existsSync } from 'fs';
 import { spawn, spawnSync } from 'child_process';
 import { Tail } from 'tail';
 import {simplefileDownload} from './lib/downloader';
 
-// clear file
-const file = "./child/input.txt";
-closeSync(openSync(file, 'w'));
+async function main(){
 
-// create websocket
-const wss = new WebSocketServer({port: 8080});
-// create logwatcher
-const tail = new Tail(file);
-
-console.log("Sarting Minecraft-Server...");
-
-// start minecraft server
-const mc_server = spawn('java', ['-jar', 'server.jar', 'nogui'], {cwd: "./child"});
-
-tail.on(("line"), function(line){
-  if(line.match(/.*FATAL.*/g) || (line.match(/.*ERROR.*/g) && line.match(/.*JMX.*/g) === null)){
-    console.log(line.match(/.*ERROR.*/g));
-    mc_server.kill();
-    // remove comman errors and kill node, docker will restart it
-    spawnSync('rm', ['-f', 'world/session.lock'], {cwd: "./child"});
-    spawnSync('killall', ['java']);
-    spawnSync('killall', ['node']);
+  // Prepare if not already happened
+  if (!existsSync("./child/server.jar")){
+    const type = "Forge";
+    const version = "1.15.2";
+    const download = await simplefileDownload(type, version);
+    if(!download){
+      process.exit();
+    }
+  }else{
+    console.log("Server already downloaded")
   }
-});
 
-// start loglistener
-tail.watch();
+  // clear file
+  const file = "./child/input.txt";
+  closeSync(openSync(file, 'w'));
 
-// clear logfile
-writeFile(file, "", (err)=>{
-  if(err) throw err;
-});
+  // create websocket
+  const wss = new WebSocketServer({port: 8080});
+  // create logwatcher
+  const tail = new Tail(file);
 
-// function on error
-mc_server.stderr.on('data', (data)=>{
-  let text = data.toString();
-  console.log(text);
-  appendFile(file, text, (err)=>{
+  console.log("Sarting Minecraft-Server...");
+
+  // start minecraft server
+  const mc_server = spawn('java', ['-jar', 'server.jar', 'nogui'], {cwd: "./child"});
+
+  tail.on(("line"), function(line){
+    if(line.match(/.*FATAL.*/g) || (line.match(/.*ERROR.*/g) && line.match(/.*JMX.*/g) === null) || line.match(/.*Server Shutdown.*/)){
+      mc_server.kill();
+      // remove comman errors and kill node, docker will restart it
+      spawnSync('rm', ['-f', 'world/session.lock'], {cwd: "./child"});
+      spawnSync('killall', ['java']);
+      process.exit();
+    }
+  });
+
+  // start loglistener
+  tail.watch();
+
+  // clear logfile
+  writeFile(file, "", (err)=>{
     if(err) throw err;
   });
-});
 
-// function on stdout
-mc_server.stdout.on('data', (data)=>{
-  let text = data.toString();
-  console.log(text);
-  appendFile(file, text, (err)=>{
-    if(err) throw err;
-  });
-});
-
-console.log("Starting WebSocket...");
-
-// start websocket
-wss.on('connection', function connection(ws) {
-
-  // happened on incomming message
-  ws.on('message', function message(data) {
+  // function on error
+  mc_server.stderr.on('data', (data)=>{
     let text = data.toString();
-    // write text to file
-    appendFile(file, ">> " + text + "\n", (err)=>{
+    console.log(text);
+    appendFile(file, text, (err)=>{
       if(err) throw err;
     });
-
-    // send text to childproccess
-    mc_server.stdin.write(text + "\n");
   });
 
-  // send new lines to websocket
-  tail.on(("line"), function(line){
-    ws.send((line));
+  // function on stdout
+  mc_server.stdout.on('data', (data)=>{
+    let text = data.toString();
+    console.log(text);
+    appendFile(file, text, (err)=>{
+      if(err) throw err;
+    });
   });
 
-  // send file on first connection
-  ws.send(readFileSync(file, {encoding:'utf8', flag:'r'}));
-});
+  console.log("Starting WebSocket...");
 
-console.log("WebSocket is running on: http://localhost:8080/");
+  // start websocket
+  wss.on('connection', function connection(ws) {
+
+    // happened on incomming message
+    ws.on('message', function message(data) {
+      let text = data.toString();
+      // write text to file
+      appendFile(file, ">> " + text + "\n", (err)=>{
+        if(err) throw err;
+      });
+
+      // send text to childproccess
+      mc_server.stdin.write(text + "\n");
+    });
+
+    // send new lines to websocket
+    tail.on(("line"), function(line){
+      ws.send((line));
+    });
+
+    // send file on first connection
+    ws.send(readFileSync(file, {encoding:'utf8', flag:'r'}));
+  });
+
+  console.log("WebSocket is running on: http://localhost:8080/");
+}
+
+main();
--- lib/downloader.ts
+++ lib/downloader.ts
@@ -1,10 +1,22 @@
 import puppeteer from 'puppeteer';
-import path from 'path';
+import {resolve} from 'path';
 import download from 'download';
+import { spawnSync } from 'child_process';
+import { unlinkSync, rename, writeFileSync, readdirSync } from 'fs';
 
-const downloadPath = path.resolve('../child');
+export async function simplefileDownload(type: string, version: string) {
+    const downloadPath = resolve('./child');
 
-export async function simplefileDownload(version, type) {
+    const eula = true;
+
+    if(eula){
+      console.log("Downloading Minecrat-Server...");
+      writeFileSync(downloadPath + "/eula.txt", "eula=true");
+    }else{
+      console.log("Please accept the EULA!");
+      process.exit();
+    }
+
     const browser = await puppeteer.launch({
         headless: true
     });
@@ -12,7 +24,7 @@ export async function simplefileDownload(version, type) {
     const page = await browser.newPage();
 
     // function to load the page
-    async function goToPage(url){
+    async function goToPage(url: string){
       await page.goto(
           url,
           { waitUntil: 'networkidle2' }
@@ -36,34 +48,53 @@ export async function simplefileDownload(version, type) {
     }
 
     // create url
-    let url
+    let url: string|object
     switch(type){
       case "Spigot":
-        url = '' + version;
-        await goToPage(url);
-
+        url = 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar';
+        await download(url, downloadPath);
+        console.log("Building Spigot...");
+        spawnSync("java", ["-jar", downloadPath + "/BuildTools.jar", "--compile", "SPIGOT", "--rev", version], {cwd: downloadPath, shell: "/bin/bash"});
+        unlinkSync(downloadPath + "/BuildTools.jar");
+        rename(downloadPath + "/spigot-" + version + ".jar", downloadPath + "/server.jar", ()=>{});
         break;
       case "Forge":
-        url = "https://files.minecraftforge.net/net/minecraftforge/forge/index_" + version + ".html";
-        await goToPage(url);
-        await page.click('body');
-        const frame = page.frames()[3];
-        await frame.click('button[title="Accept"]');
-        let href = await getHref(await page.$$('a[title="Installer"]'))
-        url = href[0].slice(href[0].lastIndexOf('=') + 1);
-        download(url, downloadPath);
+        let unSupportet = ["1.17", "1.17.1", "1.18", "1.18.1"];
+        if(!unSupportet.includes(version)){
+          url = "https://files.minecraftforge.net/net/minecraftforge/forge/index_" + version + ".html";
+          await goToPage(url);
+          await page.click('body');
+          const frame = page.frames()[3];
+          await frame.click('button[title="Accept"]');
+          let href = await getHref(await page.$$('a[title="Installer"]'))
+          url = href[0].slice(href[0].lastIndexOf('=') + 1);
+          await download(url, downloadPath);
+          console.log("Building Forge...");
+          spawnSync("java", ["-jar", downloadPath + "/forge-*-installer.jar", "--installServer"], {cwd: downloadPath, shell: "/bin/bash"});
+          //rename(downloadPath + "/minecraft_server." + version + ".jar", downloadPath + "/server.jar", ()=>{});
+          const files = readdirSync(downloadPath);
+          files.forEach((file)=>{
+            if(file.match(/forge-.*installer\.jar/)){
+              unlinkSync(downloadPath + "/" + file);
+            }else if(file.match(/forge-.*\.jar/)){
+              rename(downloadPath + "/" + file, downloadPath + "/server.jar", ()=>{});
+            }
+          });
 
+        }else{
+          console.log("This Forge version is not supportet yet!");
+          console.log("Unsupportet Forge versions are: " + unSupportet);
+          return 0;
+        }
         break;
       default:
         url = 'https://mcversions.net/download/' + version;
         await goToPage(url);
-
         url = await getHref(await page.$x("//a[contains(., 'Download Server Jar')]"));
-        download((url[0] as string), downloadPath);
+        await download((url[0] as string), downloadPath);
         break;
     }
 
     browser.close();
+    return 1;
 }
-
-simplefileDownload("1.17.1", "Forge")
